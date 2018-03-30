5b6d14921a54d40823a69273a0d37dde24d63318
DevPauloSilva <paulo_silvalearn@hotmail.com>
2018-03-29 22:01:16 -0300

    Added validation and generation of Visa card

--- lib/card_validator/cards/visa.rb
+++ lib/card_validator/cards/visa.rb
@@ -3,11 +3,11 @@ module CardValidator
     class Visa
 
       def self.validate(card_number)
-
+        /^4\d{15}/.match(card_number)
       end
       
       def self.generate
-
+        "4#{rand(10**15)}"
       end
     end
   end
