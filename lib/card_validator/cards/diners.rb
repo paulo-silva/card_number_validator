d59b70c9747f44e2dc9ece4b35769b2bf1c6daf6
DevPauloSilva <paulo_silvalearn@hotmail.com>
2018-03-29 21:25:12 -0300

    Added validation and generation of Diners card

--- lib/card_validator/cards/diners.rb
+++ lib/card_validator/cards/diners.rb
@@ -3,11 +3,11 @@ module CardValidator
     class Diners
 
       def self.validate(card_number)
-
+        card_number ? /^3\d{13}/.match(card_number) : false
       end
       
       def self.generate
-
+        "3#{rand(10**13)}"
       end
     end
   end
