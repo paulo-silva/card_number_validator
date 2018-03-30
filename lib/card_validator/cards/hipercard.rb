9b46571f2f5536211a40edbf2132bc1d4b32e9f9
DevPauloSilva <paulo_silvalearn@hotmail.com>
2018-03-29 21:57:57 -0300

    Added validation and generation of Hipercard card

--- lib/card_validator/cards/hipercard.rb
+++ lib/card_validator/cards/hipercard.rb
@@ -3,11 +3,16 @@ module CardValidator
     class Hipercard
 
       def self.validate(card_number)
-
+        return unless card_number
+        [
+          /^606282\d{10}$/,
+          /^3841(0|4|6)0\d{13}$/
+        ].each { |rgx| return true if rgx.match(card_number) }
+        false
       end
       
       def self.generate
-
+        "606282#{rand(10**10)}"
       end
     end
   end
